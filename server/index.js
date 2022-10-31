e0e57c6390aa1a6d5d8976b8cb973b4cc9408123
fitz95 <fitzgeraldnsong@gmail.com>
2022-10-31 05:40:12 +0100

    front end setup done and as well as setting up some routes. The server side has also been configured and initial socket installed as well as server requirements such as Cors, express and Dotenv

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors()); //  cors middleware
+
+const server = http.createServer(app);
+//Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
+const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST'],
+    },
+  });
+
+
+
+
+  
+server.listen(4000, () => 'Server is running on port 4000')
